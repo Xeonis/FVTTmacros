926c70ae51a26e94689d6793d58870101dec7a6c
Xeonis <m_vs@tut.by>
2024-01-31 15:20:22 +0300

    colored fixes

--- on_ulterior_tides/removeMapTiles.js
+++ on_ulterior_tides/removeMapTiles.js
@@ -1,24 +1,21 @@
 const tagsToDelete = ["mapTile", "canBeDeleted"];
 
 void async function main() {
-    let message = "Очистка текущей карты начата"
-    ui.notifications.info(message)
+    ui.notifications.info("Очистка текущей карты начата")
     try {
         const currentScene = game.scenes.current;
         const tiles = Tagger.getByTag(tagsToDelete)
         const tilesId = tiles.map(tile => tile._id)
         if (!tilesId.length) {
-            message = "Карта была пустая"
+            ui.notifications.info("Карта была пустая")
             return 0
         }
         await currentScene.deleteEmbeddedDocuments("Tile", tilesId);
-        message = `Карта очищена. Удалено: ${tilesId.length}`
+        ui.notifications.info(`Карта очищена. Удалено: ${tilesId.length} ячеек`)
         return 0
     } catch (error) {
-        message =  "что то пошло не так :("
+        ui.notifications.error("что то пошло не так :(")
         console.log(error);
         return 0
-    }finally{
-        ui.notifications.info(message)
     }
 }()
