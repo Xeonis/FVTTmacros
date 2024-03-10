9edde74afec7814c9a9c00bdc6b67eeb6cdc88c5
xeonisMainPC <om.nomm110@gmail.com>
2024-03-10 17:27:14 +0300

    place sleep correct

--- on_ulterior_tides/removeMapTiles.js
+++ on_ulterior_tides/removeMapTiles.js
@@ -6,8 +6,6 @@ function sleep(ms) {
 
 void async function main() {
     ui.notifications.info("Очистка текущей карты начата")
-// the timeout for next parts, for keep the messages order
-    await sleep(2000);
     try {
         const currentScene = game.scenes.current;
         const tiles = Tagger.getByTag(tagsToDelete)
@@ -17,14 +15,20 @@ void async function main() {
             return 0
         }
         await currentScene.deleteEmbeddedDocuments("Tile", tilesId);
+        // the timeout for next parts, for keep the messages order
+        await sleep(10000);
         ui.notifications.info(`Карта очищена. Удалено: ${tilesId.length} ячеек`)
         return 0
     } catch (error) {
         ui.notifications.error("что то пошло не так :(")
         console.log(error);
         return 0
+    }finally {
+        try {
+            // the timeout for possible next script in chain executions
+            await sleep(10000);
+        } catch {}
     }
 }()
 
-// the timeout for possible next script in chain executions
-await sleep(10000);
+
